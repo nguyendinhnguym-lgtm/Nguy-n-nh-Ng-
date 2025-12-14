73376e6a8d738026d20061d635fe80f188a2ebf3
nguyendinhnguym-lgtm <nguyendinhnguym@gmail.com>
2025-12-14 20:55:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Nguyễn Đình Ngữ - Trường PTDTBT Tiểu học Mậu Long - ĐT: 03460404620346040462</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Nguyễn Đình Ngữ - Trường PTDTBT Tiểu học Mậu Long - ĐT: 0346040462</div>", unsafe_allow_html=True)
